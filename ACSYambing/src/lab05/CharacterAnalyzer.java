438599bf7f139c7c26945f1fa401c09d338543fc
779462 <779462@apps.district196.org>
2015-10-06 09:15:26 -0500

    Beginning Lab05d

---
+++ ACSYambing/src/lab05/CharacterAnalyzer.java
@@ -0,0 +1,78 @@
+package lab05;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class CharacterAnalyzer
+{
+	private char theChar;
+
+	public CharacterAnalyzer()
+	{
+theChar=0;
+
+	}
+
+	public CharacterAnalyzer(char c)
+	{
+
+theChar=c;
+	}
+
+	public void setChar(char c)
+	{
+
+theChar=c;
+	}
+
+	public char getChar()
+	{
+		return '-';
+	}
+
+	public boolean isUpper( )
+	{
+if(theChar>=65||theChar<=90)
+{return true;
+}
+		return false;
+	}
+
+	public boolean isLower( )
+	{
+		if(theChar>=97||theChar<=122)
+		{return true;
+		}
+		return false;
+	}
+	
+	public boolean isNumber( )
+	{
+		if(theChar>=48||theChar<=57)
+		{return true;
+		}
+
+
+		return false;
+	}	
+
+	public int getASCII( )
+	{
+		return 0;
+	}
+
+	public String toString()
+	{
+
+
+
+
+		return ""+getChar() + " is a lowercase character. ASCII == " + getASCII() + "\n";	  
+	}
+}
--- ACSYambing/src/lab05/Lab05c.java
+++ ACSYambing/src/lab05/Lab05c.java
@@ -12,5 +12,12 @@ public class Lab05c
 {
 	public static void main ( String[] args )
 	{
+		Scanner scan = new Scanner(System.in);
+		out.print("Enter a word:: ");
+		String word= scan.next();
+		if(word.length()%2==0)
+		{	out.print(word+ " is even");
+		}
+		else out.print(word + " is odd");
 	}
 }
