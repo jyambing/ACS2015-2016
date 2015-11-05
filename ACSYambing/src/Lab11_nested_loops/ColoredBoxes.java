fcdcfdce9e1808e41a8bbcfe4b4dc1bec3b5d369
779462 <779462@apps.district196.org>
2015-11-05 09:44:27 -0600

    Finished Lab11g; Starting Lab11i

---
+++ ACSYambing/src/Lab11_nested_loops/ColoredBoxes.java
@@ -0,0 +1,40 @@
+package Lab11_nested_loops;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class - 
+//Lab  -
+
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Canvas;
+
+class ColoredBoxes extends Canvas
+{
+	public ColoredBoxes()
+	{
+		setBackground(Color.WHITE);
+	}
+
+	public void paint( Graphics window )
+	{
+		window.setColor(Color.BLUE);
+		window.setFont(new Font("TAHOMA",Font.BOLD,12));
+	  	window.drawString("Graphics Lab Lab11g ", 20, 40 );
+	  	window.drawString("Drawing boxes with nested loops ", 20, 80 );
+
+	  	drawBoxes(window);
+	}
+
+	public void drawBoxes(Graphics window)
+	{
+		//nested loops to draw the pretty boxes
+for(int r=0; r<9; r++){
+	for(int c=0; c<9; c++){
+		window.fillRect((r*15)+100, (c*15)+100, 10,10);
+		}
+	}
+	}
+}
---
+++ ACSYambing/src/Lab11_nested_loops/Lab11i.java
@@ -0,0 +1,17 @@
+package Lab11_nested_loops;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class - 
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Lab11i
+{
+ public static void main(String args[])
+ {
+	
+	}
+}
