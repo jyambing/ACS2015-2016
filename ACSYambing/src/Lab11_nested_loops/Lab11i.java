2c54b174c4de0f6384dab76a40d54040346c7018
779462 <779462@apps.district196.org>
2015-11-06 14:20:37 -0600

    Still working on Lab11i; Got the slanted look for each side of the triangle but I have to now combine it together.

--- ACSYambing/src/Lab11_nested_loops/Lab11i.java
+++ ACSYambing/src/Lab11_nested_loops/Lab11i.java
@@ -8,10 +8,24 @@ package Lab11_nested_loops;
 
 import static java.lang.System.*;
 
+import java.util.Scanner;
+
 public class Lab11i
 {
- public static void main(String args[])
- {
-	
-	}
-}
+	 public static void main( String args[] )
+	 {
+		 	Scanner keyboard = new Scanner(System.in);
+		 	String choice="";
+				do{
+					out.print("Enter the word you want as your triangle : ");
+					String wordy = keyboard.next();
+
+					TriangleWord tword= new TriangleWord(wordy);
+						//static methods can be called using the class name
+					System.out.println(tword);
+
+					System.out.print("Do you want to enter a new word? ");
+					choice=keyboard.next();
+				}while(choice.equals("Y")||choice.equals("y"));
+			}
+		}
--- ACSYambing/src/Lab11_nested_loops/TriangleWord.java
+++ ACSYambing/src/Lab11_nested_loops/TriangleWord.java
@@ -14,43 +14,36 @@ class TriangleWord
 
 	public TriangleWord()
 	{
-
+word="";
 	}
 
 	public TriangleWord(String w)
 	{
-
+word=w;
 	}
 
 	public void setWord(String w)
 	{
-
+word=w;
 	}
 
 	public String toString()
 	{
 		String output="";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+for(int x=1; x<=word.length(); x++)
+{
+	for(int y=0; y<word.length()-x; y++)
+	{ output+=" ";
+	}
+	output+=word.substring(x-1, x)+"\n";
+}
+for(int x=1; x<=word.length(); x++)
+{
+	for(int y=0; y<x; y++)
+	{ output+=" ";
+	}
+	output+=word.substring(x-1, x)+"\n";
+}
 		return output+"\n";
 	}
 }
