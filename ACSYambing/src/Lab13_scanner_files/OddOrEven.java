bd8b308d2fd100ba0c0fc3b15c584b61fbb576c7
779462 <779462@apps.district196.org>
2015-11-11 09:14:58 -0600

    Started Lab13 scanner labs!

---
+++ ACSYambing/src/Lab13_scanner_files/OddOrEven.java
@@ -0,0 +1,50 @@
+package Lab13_scanner_files;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class - 
+//Lab  -
+
+import static java.lang.System.*;
+
+public class OddOrEven
+{
+	private int number;
+
+	public OddOrEven()
+	{
+
+number=0;
+	}
+
+	public OddOrEven(int num)
+	{
+
+number=num;
+	}
+
+	public void setNum(int num)
+	{
+
+number=num;
+	}
+
+	public boolean isOdd( )
+	{
+		boolean result= true;
+
+if(number %2==0)
+ result = false;
+		return result;
+	}
+
+	public String toString()
+	{
+String statement =number+" is even.\n\n";
+ if(isOdd()==false){
+	statement= number+ " is odd. \n\n";
+	}
+ return statement;
+}
+}
