618f09be2aefe2b2b0091ced4ae5869c2f275021
779462 <779462@apps.district196.org>
2016-01-14 09:42:01 -0600

    Started Maps!

---
+++ ACSYambing/src/maps/Lab08a.java
@@ -0,0 +1,33 @@
+package maps;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class Lab08a
+{
+	public static void main( String args[] ) throws IOException
+	{
+		Scanner file = new Scanner(new File("lab08a.dat"));
+
+		SpanishToEnglish test = new SpanishToEnglish();
+
+		//read in pairs from the file and load the map
+
+		out.println("\n====\tMAP CONTENTS\t====\n\n");
+
+		//print the map
+
+		out.println(test+"\n\n");
+
+		//read in the lines and convert them
+
+	}
+}
