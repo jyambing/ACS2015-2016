4c79343381ff822315ee628c4e8729cea706a0ed
779462 <779462@apps.district196.org>
2016-01-27 15:04:58 -0600

    Finished Histogram!!!

--- ACSYambing/src/maps/Histogram.java
+++ ACSYambing/src/maps/Histogram.java
@@ -1,51 +1,61 @@
 package maps;
 //© A+ Computer Science  -  www.apluscompsci.com
+
 //Name -
 //Date -
 //Class -
 //Lab  -
 
 import java.util.Map;
+import java.io.IOException;
 import java.util.TreeMap;
 import java.util.Scanner;
 import static java.lang.System.*;
 
-public class Histogram
-{
-	private Map<String,Integer> histogram;
+public class Histogram {
+	private Map<String, Integer> histogram;
 
-	public Histogram()
-	{
+	public Histogram() {
+		
+		setSentence("");
 	}
 
-	public Histogram(String sent)
-	{
+	public Histogram(String sent) {
+		this();
 		setSentence(sent);
 	}
-	
-	public void setSentence(String sent)
-	{
-		histogram= new TreeMap<String, Integer>();
-		String [] stuff= sent.split(" ");
-		for(int x=0; x<stuff.length; x++)
-			if(histogram.get(stuff[x])==null)
-			histogram.put(stuff[x], 0); 
-			else
-			histogram.put(stuff[x],histogram.get(stuff[x]+1));
+
+	public void setSentence(String sent) {
+		histogram = new TreeMap<String, Integer>();
+
+		String[] stuff = sent.split(" ");
+		for (int x = 0; x < stuff.length; x++){
+			if (histogram.get(stuff[x]) == null)
+				histogram.put(stuff[x], 0);
+				histogram.put(stuff[x], histogram.get(stuff[x]) + 1);
+	}
 	}
 
-	public String toString()
-	{
-		String output="";
-		String allStars="";
-		output= "char    1---5----01---5\n\n";
-	for(String charName: histogram.keySet()){
-		output+= charName+"\t";
-		for(int y=0; y<histogram.get(charName); y++){
-			output+="*";
+	public String toString() {
+		String output = "";
+		output += "char    1---5----01---5\n";
+		for (String s : histogram.keySet()) {
+			output += s + "\t";
+			String allStars = "";
+			for (int x = 0; x < histogram.get(s); x++) {
+				allStars += "*";
+			}
+			output += allStars + "\n";
 		}
-			output+= "\n";
+
+		/*
+		 * String output=""; String allStars=""; output+=
+		 * "char    1---5----01---5\n"; for(String charName:
+		 * histogram.keySet()){ output+= charName+"\t"; for(int y=0;
+		 * y<histogram.get(charName); y++){ allStars+="*"; } output+=allStars+
+		 * "\n";
+		 */
+
+		return output + "\n\n";
 	}
-	return output+"\n\n";
-}
 }
--- ACSYambing/src/maps/Lab08b.java
+++ ACSYambing/src/maps/Lab08b.java
@@ -19,9 +19,10 @@ public class Lab08b
 		String test1= "a b c d e f g h i a c d e g h i h k";
 		Histogram test = new Histogram(test1);
 		//read in pairs from the file and load the map
+		//test.setSentence(test1);
 		out.println(test);
 		//print the map
-		String test2= "1 2 3 4 5 6 1 2 3 4 5 1 3 1 2 3 4";
+		/*String test2= "1 2 3 4 5 6 1 2 3 4 5 1 3 1 2 3 4";
 		Histogram t2= new Histogram(test2);
 		out.println(t2);
 		
@@ -31,7 +32,7 @@ public class Lab08b
 		
 		String test4= "4 T # @ ^ # # #";
 		Histogram t4= new Histogram(test4);
-		out.println(t4);
+		out.println(t4);*/
 		
 	}
 }
