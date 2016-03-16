cdb7fde40c56cfcddcf018a337eb331ace007534
779462 <779462@apps.district196.org>
2016-03-16 09:15:55 -0500

    Still working on syntax checker; specifically the checkExpression method

--- ACSYambing/src/stacks/SyntaxChecker.java
+++ ACSYambing/src/stacks/SyntaxChecker.java
@@ -16,20 +16,58 @@ public class SyntaxChecker
 
 	public SyntaxChecker()
 	{
-		symbols= new Stack<Characters>();
+		setExpression("");
 	}
 
 	public SyntaxChecker(String s)
 	{
+		symbols= new Stack<Character>();
+		setExpression(s);
 	}
 	
 	public void setExpression(String s)
 	{
+		exp=s;
+		char[] stacky= new char[s.length()];
+		for(int x=0; x<s.length(); x++)
+		{
+			stacky[x]=s.charAt(x);
+		}
+		for(char i: stacky)
+		{
+			symbols.push(i);
+		}
 	}
 
 	public boolean checkExpression()
 	{
+		int brak1, brak2, ar1, ar2;
+		while(symbols.empty()!=true)
+		{
+		 if(symbols.peek().equals("{"){
+		 brak1++;
+		 }
+		 else if(symbols.peek().equals("}"){
+				 brak2++;
+		 }
+		 else if(symbols.peek().equals("["){
+				 ar1++;
+		 }
+		 else if(symbols.peek().equals("]"){
+				 ar2++;
+		 }
+		}
+		
+		
 		return false;
+	}
+	public String toString()
+	{
+		if(symbols.checkExpression())
+		{
+			return exp+ " "+ "is correct";
+		}
+		else return exp+ " "+ "is incorrect";
 	}
 
 	//write a toString
