3a04061d12a4912f5fde1571667ddb917404b1a6
779462 <779462@apps.district196.org>
2016-03-30 09:16:10 -0500

    Finished postfix need to write runner

--- ACSYambing/src/stacks/PostFix.java
+++ ACSYambing/src/stacks/PostFix.java
@@ -11,7 +11,8 @@ import static java.lang.System.*;
 
 public class PostFix
 {
-	private Stack<Double> stack;
+	private Stack<Double> nums;
+	private Stack<Character> ops;
 	private String expression;
 
 	public PostFix()
@@ -21,24 +22,50 @@ public class PostFix
 
 	public PostFix(String exp)
 	{
-		stack= new Stack<Double>();
+		nums= new Stack<Double>();
+		ops= new Stack<Character>();
 		setExpression(exp);
 	}
 
 	public void setExpression(String exp)
 	{
 		expression=exp;
+		String[] split= expression.split(" ");
+		for(int x=0; x<split.length; x++)
+		{
+			if(split[x].charAt(0)>=48 && split[x].charAt(0)<=57)
+				nums.push(Double.parseDouble(split[x]));
+			else
+				ops.push(split[x].charAt(0));
+		}
 	}
 
-	public double calc(double one, double two, char op)
+	public double calc()
 	{
-		
-		return 0.0;
+		double output= nums.pop();
+		for(Character x: ops)
+		{
+			if(x=='+')
+				output+=nums.pop();
+			if(x=='-')
+				output-=nums.pop();
+			if(x=='*')
+				output*=nums.pop();
+			if(x=='/')
+				output/=nums.pop();
+			if(x=='%')
+				output%=nums.pop();
+		}
+		return output;
 	}
 
 	public void solve()
 	{
 	}
 
+	public String toString()
+	{
+		return "" +this.calc();
+	}
 	//add a toString
 }
