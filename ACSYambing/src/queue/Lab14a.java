f04cf361ea3b73d10013fdaae5d4caf045ed8a1b
779462 <779462@apps.district196.org>
2016-04-01 09:21:05 -0500

    Finsishing PalinList

--- ACSYambing/src/queue/Lab14a.java
+++ ACSYambing/src/queue/Lab14a.java
@@ -10,6 +10,23 @@ public class Lab14a
 {
 	public static void main ( String[] args )
 	{
+		PalinList one= new PalinList("one two three two one");
+		System.out.println(one);
+		
+		PalinList two= new PalinList("1 2 3 4 5 one two three four five");
+		System.out.println(two);
+		
+		PalinList three= new PalinList("a b c d e f g x y z g f h");
+		System.out.println(three);
+		
+		PalinList four= new PalinList("racecar is racecar");
+		System.out.println(four);
+		
+		PalinList five= new PalinList("1 2 3 a b c c b a 3 2 1");
+		System.out.println(five);
+		
+		PalinList six= new PalinList("chicken is a chicken");
+		System.out.println(six);
 		//add test cases			
 	}
 }
--- ACSYambing/src/queue/PalinList.java
+++ ACSYambing/src/queue/PalinList.java
@@ -1,5 +1,6 @@
 package queue;
 //© A+ Computer Science  -  www.apluscompsci.com
+
 //Name -
 //Date -
 //Class -
@@ -11,40 +12,54 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-public class PalinList
-{
+public class PalinList {
 	private Queue<String> queue;
 	private Stack<String> stack;
 
-	public PalinList()
-	{
+	public PalinList() {
+		stack = new Stack<String>();
+		queue = new LinkedList<String>();
 		setList("");
 	}
 
-	public PalinList(String list)
-	{
+	public PalinList(String list) {
+		stack = new Stack<String>();
+		queue = new LinkedList<String>();
 		setList(list);
 	}
 
-	public void setList(String list)
-	{
-		stack= new Stack<String>();
-		String[] stacky= list.split(" ");
-		for(String i: stacky)
-		{
-			stack.push(i);
+	public void setList(String list) {
+		queue.clear();
+		stack.clear();
+		String[] stacky = list.split(" ");
+		for (String i : stacky) {
+			stack.add(i);
 			queue.add(i);
-			
+
 		}
 	}
 
-	public boolean isPalin()
-	{
-		for(String x: stack)
-			if(stack.peek().)
+	public boolean isPalin() {
+		while (stack.isEmpty() != false&&queue.isEmpty()!=false) {
+			
+			String p =stack.pop();
+			System.out.println(p);
+			String q=queue.poll();
+			if (!p.equals(q)) {
+				return false;
+			}
+
+		}
 		return true;
 	}
 
+	public String toString() {
+		if (isPalin() == true)
+			return stack + " is a palinlist";
+		else
+			return stack + " is not a palinlist";
 
-	//write a toString method
+	}
+
+	// write a toString method
 }
