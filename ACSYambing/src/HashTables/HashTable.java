537c028715f01bf557350a93a6cf4c10d3bb61b7
779462 <779462@apps.district196.org>
2016-04-20 09:10:43 -0500

    Starting hashtables!

---
+++ ACSYambing/src/HashTables/HashTable.java
@@ -0,0 +1,48 @@
+package HashTables;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class - 
+//Lab  -
+
+import java.util.LinkedList;
+import java.util.Scanner;
+import static java.lang.System.*;
+
+public class HashTable
+{
+	private LinkedList[] table;
+
+	public HashTable( )
+	{
+		table = new LinkedList[10];
+	}
+
+	public void add(Number obj)
+	{
+		System.out.println("add");
+		int i = obj.hashCode();
+
+
+
+
+
+	}
+
+	public String toString()
+	{
+		String output="HASHTABLE\n";
+
+
+
+
+
+
+
+
+
+
+		return output;
+	}
+}
---
+++ ACSYambing/src/HashTables/Number.java
@@ -0,0 +1,37 @@
+package HashTables;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class - 
+//Lab  -
+
+public class Number
+{
+	private int theValue;
+	
+	public Number(int value)
+	{
+		theValue= value;
+	}	
+	
+	public int getValue()
+	{
+		return theValue;
+	}
+	
+	public boolean equals(Number obj)
+	{
+		return getValue() == obj.getValue();
+	} 
+	
+	public int hashCode()
+	{
+		return theValue%10;
+	}
+
+	public String toString()
+	{
+		return ""+theValue;
+	}	
+}
