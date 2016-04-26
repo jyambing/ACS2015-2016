b661471477e73b514a01dae88b330280d40fc3b9
779462 <779462@apps.district196.org>
2016-04-26 09:14:43 -0500

    Starting second hashtable!

---
+++ ACSYambing/src/HashTables/Word.java
@@ -0,0 +1,31 @@
+package HashTables;
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class - 
+//Lab  -
+
+public class Word
+{
+	private String theValue;
+	
+	//write a constructor method
+	
+	
+	
+	//write the getValue method
+	
+	
+	
+	//write the equals method
+	
+	
+	
+	//write the hashCode method
+	
+	
+	
+	//write the toString method
+
+	
+}
