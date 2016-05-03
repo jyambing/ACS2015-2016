b8203b09f926d880a6667b45bf864515032d4731
779462 <779462@apps.district196.org>
2016-05-03 09:10:42 -0500

    started binary trees!

---
+++ ACSYambing/src/binaryTrees/BSTreeRunner.java
@@ -0,0 +1,25 @@
+package binaryTrees;
+
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -  
+//Date -
+//Class - 
+//Lab  -
+
+import static java.lang.System.*;
+
+public class BSTreeRunner
+{
+ public static void main( String args[] )
+ {
+		//add test cases here
+		
+		
+		
+		
+		
+		
+		
+
+ }
+}
